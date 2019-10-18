d3f020b5fdf74ce35ca62d241013262ba8bde112
willy-michaud <85willy85@gmail.com>
2019-10-18 14:42:43 +0200

    code

--- Programme/SUPERQUIZ4000/image.h
+++ Programme/SUPERQUIZ4000/image.h
@@ -1,4 +1,18 @@
 #ifndef IMAGE_H
 #define IMAGE_H
+#include <stdio.h>
+class IMAGE_H {
+
+public:
+
+image();
+//image();
+
+private:
+
+int m_taille;
+std::string m_nom ;
+std::string m_description;
+};
 
 #endif // IMAGE_H
