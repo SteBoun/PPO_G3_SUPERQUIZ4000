72d7f10c4d2e5ea84e669d355dbfb5c5dd1b59ab
willy-michaud <85willy85@gmail.com>
2019-10-18 14:52:24 +0200

    Maj code

--- Programme/SUPERQUIZ4000/image.h
+++ Programme/SUPERQUIZ4000/image.h
@@ -10,9 +10,9 @@ image();
 
 private:
 
-int m_taille;
-std::string m_nom ;
-std::string m_description;
+int m_definition;//variable contenant la définition de l'image
+std::string m_nom ;//variable contenant le nom de l'image
+std::string m_description;//variable contenant la description de l'image
 };
 
 #endif // IMAGE_H
